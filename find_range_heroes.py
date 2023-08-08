ac6394715d000cc047aa24eb4c25c2e29ec88c10
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-08-08 00:45:12 -0400

    fixed grammar issues

--- find_range_heroes.py
+++ find_range_heroes.py
@@ -7,8 +7,8 @@ url = "https://api.opendota.com/api/heroes"
 data = requests.get(url).json()
 
 ranged_heroes = 0
-for heroe in data:
-    if heroe['attack_type'] == "Ranged":
-        print(heroe["id"], heroe["attack_type"], heroe["localized_name"])
+for hero in data:
+    if hero['attack_type'] == "Ranged":
+        print(hero["id"], hero["attack_type"], hero["localized_name"])
         ranged_heroes +=1
 print(f"The number of heroes is {ranged_heroes}")
