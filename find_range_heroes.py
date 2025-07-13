a380279537869381343df2d5e9576978bef0271a
Antonio Rios <antoniorios17@users.noreply.github.com>
2025-07-12 22:03:32 -0400

    converted find ranged heroes into dataframes

--- find_range_heroes.py
+++ find_range_heroes.py
@@ -1,14 +1,22 @@
 import requests
 import json
-
+import pandas as pd
 
 url = "https://api.opendota.com/api/heroes"
 
 data = requests.get(url).json()
 
+list_of_heroes = []
 ranged_heroes = 0
 for hero in data:
     if hero['attack_type'] == "Ranged":
-        print(hero["id"], hero["attack_type"], hero["localized_name"])
+        heroes = hero["id"], hero["attack_type"], hero["localized_name"]
+        list_of_heroes.append(heroes)
         ranged_heroes +=1
-print(f"The number of heroes is {ranged_heroes}")
+# print(f"The number of heroes is {ranged_heroes}")
+
+# create df for the data
+
+df = pd.DataFrame(list_of_heroes, columns=["id", 'type', 'name']) # add column names
+
+print(df)
